56990e5e4b3999dcbf8e94a8d5778f43697d0e4f
Joao Vitor Cezario Sanches <joaovitormanolo@gmail.com>
2022-01-31 15:45:16 -0300

    Improving stylesheets, last commit before evaluation

--- scripts/main.js
+++ scripts/main.js
@@ -9,14 +9,11 @@ for(let i = 0; i < contentSource.length; i++){
     button.setAttribute('id', `group${i}`)
     for(let j = 0; j < contentSource[i].values.length; j++){
         button.setAttribute('value', `${contentSource[i].values[j].price}`)
-        button.setAttribute('data-identifier', 'food-option')
         button.querySelector('img').src = `public/${contentSource[i].values[j].name}.jpg`
         button.querySelector('img').alt = contentSource[i].values[j].name
-        button.querySelector('.title').innerText = contentSource[i].values[j].name
-        button.querySelector('.title').setAttribute('data-identifier', 'food-title')
-        button.querySelector('.description').innerText = contentSource[i].values[j].description
+        button.querySelector('button > span').innerText = contentSource[i].values[j].name
+        button.querySelector('.description span').innerText = contentSource[i].values[j].description
         button.querySelector('.price').innerText = 'R$ ' + realConversion(contentSource[i].values[j].price)
-        button.querySelector('.price').setAttribute('data-identifier', 'food-price')
         section.querySelector('article').appendChild(button)
         button = button.cloneNode(true)
     }
@@ -41,7 +38,7 @@ submitButton.addEventListener('click', () => {
 
     popupDiv.style.top = `${window.scrollY}px`
     for(let i = 0; i < selectedButtons.length; i++){
-        let foodName = selectedButtons[i].querySelector('.title').innerText
+        let foodName = selectedButtons[i].querySelector('button > span').innerText
         contentHolder.firstElementChild.innerText = foodName
         text += `- ${contentSource[i].name}: ${foodName}\n`
         contentHolder.lastElementChild.innerText = realConversion(parseFloat(selectedButtons[i].value))
